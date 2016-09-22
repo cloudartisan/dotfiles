ab057e4db0ba270354162b088b8591ab3e1a0619
David Taylor <david@cloudartisan.com>
2016-09-22 23:27:42 +1000

    Adds automatic tmuxline support

--- bin/install_vim.sh
+++ bin/install_vim.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 vim "+PluginInstall" "+qall"
+
+# Snapshot bash prompt settings for reuse
 vim "+PromptlineSnapshot! ${HOME}/.bash_promptline_airline airline" "+qall"
+
+# Snapshot tmux status settings for reuse
+vim "+TmuxlineSnapshot! ${HOME}/.tmux_tmuxline.conf" "+qall"
 
 # Compile YouCompleteMe if it's installed
 if [[ -d $HOME/.vim/bundle/YouCompleteMe ]]; then
