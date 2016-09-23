2568ae1f1ba5008104370089a37e8815dad19e42
David Taylor <david@cloudartisan.com>
2016-09-23 18:04:36 +1000

    Adds update_vim.sh

---
+++ bin/update_vim.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+vim "+PluginInstall!" "+qall"
+
+# Snapshot bash prompt settings for reuse
+vim "+PromptlineSnapshot! ${HOME}/.bash_promptline_airline airline" "+qall"
+
+# Snapshot tmux status settings for reuse
+vim "+TmuxlineSnapshot! ${HOME}/.tmux_tmuxline.conf" "+qall"
+
+# Compile YouCompleteMe if it's installed
+if [[ -d $HOME/.vim/bundle/YouCompleteMe ]]; then
+  $HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer
+fi
