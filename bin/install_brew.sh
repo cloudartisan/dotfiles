16353a5ec3f9439857c9e879d1c66099e70d5831
David Taylor <david@cloudartisan.com>
2016-09-23 12:37:30 +1000

    Adds frequently-used applications

--- bin/install_brew.sh
+++ bin/install_brew.sh
@@ -19,9 +19,20 @@ brew install tmux
 brew install awscli
 brew install terraform
 
+# Frequently used applications
+brew tap caskroom/cask
 brew cask
 brew cask install iterm2
 brew cask install google-chrome
+brew cask install google-drive
+brew cask install amazon-drive
+brew cask install synology-cloud-station-drive
+brew cask install lastpass
+brew cask install screenhero
+brew cask install slack
+brew cask install kindle
+brew cask install send-to-kindle
+brew cask install minecraft
 
 # Remove outdated versions from the cellar.
 brew cleanup
