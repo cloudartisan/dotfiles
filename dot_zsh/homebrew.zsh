8af3b547587e291dd951113c7a3989de6404b5a6
cloudartisan <david@cloudartisan.com>
2026-09-03 14:48:16 +1000

    Stop brew upgrade from reinstalling self-updating casks

--- dot_zsh/homebrew.zsh
+++ dot_zsh/homebrew.zsh
@@ -1,6 +1,11 @@
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
+# Leave self-updating apps (auto_updates casks) to their own updaters. Modern
+# brew upgrade reinstalls them by default, which churns large re-downloads and
+# can transiently downgrade an app whose updater is ahead of the Homebrew cask.
+export HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS=1
+
 # Add Homebrew-provided completions to fpath (must happen before compinit)
 if type brew &>/dev/null
 then
